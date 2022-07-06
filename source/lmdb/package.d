f2ba0744b072ca7461eefecc3c371c4f41990bac
Ikey Doherty <ikey@serpentos.com>
2022-07-06 16:58:03 +0100

    Initial structure
    Signed-off-by: Ikey Doherty <ikey@serpentos.com>

---
+++ source/lmdb/package.d
@@ -0,0 +1,16 @@
+/*
+ * SPDX-FileCopyrightText: Copyright © 2020-2022 Serpent OS Developers
+ *
+ * SPDX-License-Identifier: Zlib
+ */
+
+/**
+ * lmdb
+ *
+ * Module level imports
+ *
+ * Authors: Copyright © 2020-2022 Serpent OS Developers
+ * License: Zlib
+ */
+
+module lmdb;
